eb9d79db860b17bcb426bbfa8470cff03dccde97
Alexandr <knuckles05@yandex.ru>
2024-10-09 09:57:33 +0300

    second commit

--- source/repos/cb/cb/cb.cpp
+++ source/repos/cb/cb/cb.cpp
@@ -95,11 +95,90 @@ int main() {
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
 
-void rec(int n) {
 
+int minimum(int x, int y);
+void reverse(int number);
+void swap(int* x, int* y);
+void stupid_swap(int x, int y);
+
+int fact_iter(int n) {
+	int t;
+	int answer;
+	for (t = answer = 1; t <= n; answer *= t, t++);
+	return answer;
 }
 
-int main(int agrs, char* argv[]) {
+int fact_rec(int n) {
+	if (n == 0) {
+		return 1;
+	}
+	return n * fact_rec(n - 1);
+}
 
+int find_min(int* mas, int len);
+
+int minimum(int x, int y) {
+	if (x < y) {
+		return x;
+	}
+	else {
+		return y;
+	}
+}
+
+void reverse(int number) {
+	while (number) {
+		printf("%d", number % 10);
+		number /= 10;
+	}
+	printf("\n");
+}
+
+void stupid_swap(int x, int y) {
+	int temp;
+	printf("x = %d\ny = %d\n", x, y);
+	temp = x;
+	x = y;
+	y = temp;
+	printf("x = %d\ny = %d\n\n", x, y);
+}
+
+void swap(int* x, int* y) {
+	int temp;
+	printf("x = %d\ny = %d\n", *x, *y);
+	printf("addr x = %p\naddr y = %p\n", x, y);
+	temp = *x;
+	*x = *y;
+	*y = temp;
+	printf("x = %d\ny = %d\n\n", *x, *y);
+	printf("addr x = %p\naddr y = %p\n", x, y);
+}
+
+int find_min(int* mas, int len) {
+	int min = mas[0];
+	for (int i = 1; i < len; i++) {
+		if (mas[i] < min) {
+			min = mas[i];
+		}
+	}
+	return min;
+}
+
+
+int main(int argc, char* argv[]) {
+	setlocale(LC_ALL, "ru");
+	printf("number of params = %d\n", argc);
+	printf("program name: %s\n", argv[0]);
+	if (argc != 2) {
+		printf("Ошибка! укажите имя через пробел после вызова программы\n");
+		exit(1);
+	}
+	else {
+		printf("hi, %s\n", argv[1]);
+	}
+	return 0;
 }
